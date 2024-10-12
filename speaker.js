db637a589261c12c5e0998bf94bbcdf63fb54be3
Aaditya0112 <rajaaditya0112@gmail.com>
2024-10-13 00:01:57 +0530

    Updates

--- speaker.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.infoButton').forEach(button => {
-button.addEventListener('click', function() {
-    console.log('clicked');
-    document.querySelector('.layer').classList.toggle('activeL');
-    
-});});
-
-// document.querySelector('.popupClose').addEventListener("click", () => {
-//     document.querySelector('.layer').classList.remove('activeL');})
